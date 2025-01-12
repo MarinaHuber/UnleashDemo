f100d45310533358fabb9c9d50a944a46c219e72
marina <huber.marina@gmail.com>
2025-01-12 20:42:50 +0100

    ADDS projcet files

---
+++ UnleashFifth/UnleashFifth/UnleashFifthApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnleashFifthApp.swift
+//  UnleashFifth
+//
+//  Created by Marina Huber on 12.01.2025..
+//
+
+import SwiftUI
+
+@main
+struct UnleashFifthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UnleashFifth/UnleashFifthTests/UnleashFifthTests.swift
@@ -0,0 +1,17 @@
+//
+//  UnleashFifthTests.swift
+//  UnleashFifthTests
+//
+//  Created by Marina Huber on 12.01.2025..
+//
+
+import Testing
+@testable import UnleashFifth
+
+struct UnleashFifthTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
